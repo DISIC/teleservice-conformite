633fd0f24d2f6f5a8a5be38b891a89d0e73e7c81
Antoine Lelong <azenoxe@gmail.com>
2025-11-21 11:35:45 +0100

    feat: cleanup dashboard home page

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,65 +1,12 @@
 import { fr } from "@codegouvfr/react-dsfr";
-import { Button } from "@codegouvfr/react-dsfr/Button";
-import { useState } from "react";
 import { tss } from "tss-react";
-import z from "zod";
-import { api } from "~/utils/api";
-import { useAppForm } from "~/utils/form/context";
 
 export default function Home() {
 	const { classes } = useStyles();
 
-	const [result, setResult] = useState<any | null>(null);
-
-	const { mutateAsync: getInfoFromAra, isPending } =
-		api.declaration.getInfoFromAra.useMutation({
-			onSuccess: (data) => setResult(data),
-			onError: (error) => console.error("error", error),
-		});
-
-	const form = useAppForm({
-		defaultValues: {
-			id: "",
-		},
-		validators: {
-			onSubmit: z.object({
-				id: z.string().min(1, "L'identifiant ARA est requis"),
-			}),
-		},
-		onSubmit: async ({ value }) => {
-			getInfoFromAra(value);
-		},
-	});
-
 	return (
 		<div className={classes.main}>
-			<form
-				className={fr.cx("fr-mb-6v")}
-				onSubmit={(e) => {
-					e.preventDefault();
-					form.handleSubmit();
-				}}
-			>
-				<h2>Test Ara Informations</h2>
-				<form.AppField
-					name="id"
-					children={(field) => <field.TextField label="Identifiant ARA" />}
-				/>
-				<form.Subscribe
-					selector={(form) => [form.canSubmit]}
-					children={(canSubmit) => (
-						<Button type="submit" disabled={!canSubmit || isPending}>
-							Valider
-						</Button>
-					)}
-				/>
-			</form>
-			{result && (
-				<>
-					<h3>Résultat :</h3>
-					<pre>{JSON.stringify(result, null, 2)}</pre>
-				</>
-			)}
+			<h1>Dashboard</h1>
 		</div>
 	);
 }
