addd4065f4690eee20e8e3abbc4c21031fc2813f
linnaa <linna.azaiez@gmail.com>
2025-12-04 11:08:35 +0100

    feat: add readonly prop to form

--- src/utils/form/declaration/form.tsx
+++ src/utils/form/declaration/form.tsx
@@ -2,13 +2,13 @@ import { fr } from "@codegouvfr/react-dsfr";
 import Accordion from "@codegouvfr/react-dsfr/Accordion";
 import Button from "@codegouvfr/react-dsfr/Button";
 import Checkbox from "@codegouvfr/react-dsfr/Checkbox";
+
 import { useState } from "react";
 import { tss } from "tss-react";
 import { rgaaVersionOptions } from "~/payload/collections/Audit";
 import { appKindOptions } from "~/payload/collections/Declaration";
 import { withForm } from "../context";
 import { declarationMultiStepFormOptions } from "./schema";
-import { read } from "node:fs";
 
 const envKindOptions = [
 	{ label: "Mobile", value: "mobile" },
@@ -75,80 +75,126 @@ export const DeclarationGeneralForm = withForm({
 export const DeclarationAuditForm = withForm({
 	...declarationMultiStepFormOptions,
 	props: {
-		isAchievedCondition: false,
+		isAchieved: false,
 		readOnly: false,
 	},
-	render: function Render({ form, isAchievedCondition, readOnly }) {
+	render: function Render({ form, isAchieved: initialIsAchieved, readOnly }) {
 		const { classes, cx } = useStyles();
 
-		const [isAchieved, setIsAchieved] = useState(false);
+		const [isAchieved, setIsAchieved] = useState(initialIsAchieved);
 
 		return (
 			<div className={cx(classes.formWrapper)}>
-				{isAchievedCondition && (
+				{!readOnly ? (
 					<Checkbox
 						options={[
 							{
 								label: "L'audit d'accessibilité a-t-il été réalisé ?",
 								nativeInputProps: {
 									checked: isAchieved,
-									onChange: (e) => setIsAchieved(e.target.checked),
+									// onChange: (e) => setIsAchieved(e.target.checked),
 								},
 							},
 						]}
 						className={fr.cx("fr-mb-3w")}
 						style={{ userSelect: "none" }}
 					/>
+				) : (
+					<p>
+						<strong>Audit réalisé:</strong> {isAchieved ? "Oui" : "Non"}
+					</p>
 				)}
-				{(!isAchievedCondition || (isAchievedCondition && isAchieved)) && (
+				<span
+					style={{
+						backgroundColor: fr.colors.decisions.background.default.grey.hover,
+						height: "10px",
+						width: "100%",
+						marginBottom: fr.spacing("4w"),
+						display: "block",
+					}}
+				/>
+				{isAchieved && (
 					<>
-						<form.AppField name="audit.url">
+						<form.AppField name="audit.date">
 							{(field) => (
-								<field.TextField label="Url de l'audit" readOnly={readOnly} />
+								<field.TextField
+									label="Date de realisation"
+									kind="date"
+									max={new Date().toISOString().split("T")[0]}
+									readOnly={readOnly}
+								/>
 							)}
 						</form.AppField>
-						<form.AppField name="audit.rgaa_version">
+						<span
+							style={{
+								backgroundColor:
+									fr.colors.decisions.background.default.grey.hover,
+								height: "2px",
+								width: "100%",
+								marginBottom: fr.spacing("4w"),
+								display: "block",
+							}}
+						/>
+						<form.AppField name="audit.realisedBy">
 							{(field) => (
 								<field.RadioField
-									label="Version du RGAA utilisée"
+									label="Entité ou personne ayant réalisé l'audit"
 									options={[...rgaaVersionOptions]}
 									readOnly={readOnly}
 								/>
 							)}
 						</form.AppField>
-						<div className={classes.gridRow}>
-							<form.AppField name="audit.date">
-								{(field) => (
-									<field.TextField
-										label="Date de l'audit"
-										kind="date"
-										max={new Date().toISOString().split("T")[0]}
-										readOnly={readOnly}
-									/>
-								)}
-							</form.AppField>
-							<form.AppField name="audit.rate">
-								{(field) => (
-									<field.NumberField
-										label="Taux de conformité (%)"
-										readOnly={readOnly}
-									/>
-								)}
-							</form.AppField>
-						</div>
-						<form.AppField name="audit.realisedBy">
+						<span
+							style={{
+								backgroundColor:
+									fr.colors.decisions.background.default.grey.hover,
+								height: "2px",
+								width: "100%",
+								marginBottom: fr.spacing("4w"),
+								display: "block",
+							}}
+						/>
+						<form.AppField name="audit.rgaa_version">
 							{(field) => (
-								<field.TextField
-									label="Réalisé par l'organisation"
+								<field.RadioField
+									label="Référentiel RGAA utilisé"
+									options={[...rgaaVersionOptions]}
 									readOnly={readOnly}
 								/>
 							)}
 						</form.AppField>
-						<div className={fr.cx("fr-accordions-group")}>
-							<form.AppField name="audit.pages" mode="array">
+						<span
+							style={{
+								backgroundColor:
+									fr.colors.decisions.background.default.grey.hover,
+								height: "2px",
+								width: "100%",
+								marginBottom: fr.spacing("4w"),
+								display: "block",
+							}}
+						/>
+						<div className={classes.gridRow}>
+							<form.AppField name="audit.rate">
 								{(field) => (
+									<field.NumberField label="Résultats" readOnly={readOnly} />
+								)}
+							</form.AppField>
+						</div>
+						<span
+							style={{
+								backgroundColor:
+									fr.colors.decisions.background.default.grey.hover,
+								height: "10px",
+								width: "100%",
+								marginBottom: fr.spacing("4w"),
+								display: "block",
+							}}
+						/>
+						<form.AppField name="audit.technologies" mode="array">
+							{(field) =>
+								!readOnly ? (
 									<Accordion
-										label="Pages auditées"
+										label="Technologies utilisées (dans l'audit)"
 										defaultExpanded
 										className={classes.pagesAccordion}
 									>
@@ -158,48 +204,46 @@ export const DeclarationAuditForm = withForm({
 												className={classes.pagesAccordionContent}
 											>
 												<div className={classes.pagesWrapper}>
-													<form.AppField name={`audit.pages[${index}].label`}>
+													<form.AppField name={`audit.technologies[${index}]`}>
 														{(subField) => (
 															<subField.TextField
-																label={`Page ${index + 1} - Label`}
-																className={fr.cx("fr-mb-0")}
-																readOnly={readOnly}
-															/>
-														)}
-													</form.AppField>
-													<form.AppField name={`audit.pages[${index}].url`}>
-														{(subField) => (
-															<subField.TextField
-																label={`Page ${index + 1} - URL`}
-																readOnly={readOnly}
+																label={`Technologie ${index + 1}`}
 															/>
 														)}
 													</form.AppField>
 												</div>
-												{!readOnly && (
-													<Button
-														type="button"
-														priority="secondary"
-														iconId="fr-icon-delete-bin-line"
-														onClick={() => field.removeValue(index)}
-														title="Supprimer la page"
-													/>
-												)}
+												<Button
+													type="button"
+													priority="secondary"
+													iconId="fr-icon-delete-bin-line"
+													onClick={() => field.removeValue(index)}
+													title="Supprimer la technologie"
+												/>
 											</div>
 										))}
-										{!readOnly && (
-											<Button
-												type="button"
-												onClick={() => field.pushValue({ url: "", label: "" })}
-											>
-												Ajouter une page
-											</Button>
-										)}
+										<Button type="button" onClick={() => field.pushValue("")}>
+											Ajouter une technologie
+										</Button>
 									</Accordion>
-								)}
-							</form.AppField>
-							<form.AppField name="audit.testEnvironments" mode="array">
-								{(field) => (
+								) : (
+									<p>
+										<strong>Technologies utilisées:</strong>{" "}
+										{field.state.value.length > 0 ? (
+											<ul>
+												{field.state.value.map((value) => (
+													<li key={value}>{value}</li>
+												))}
+											</ul>
+										) : (
+											"None"
+										)}
+									</p>
+								)
+							}
+						</form.AppField>
+						<form.AppField name="audit.testEnvironments" mode="array">
+							{(field) =>
+								!readOnly ? (
 									<Accordion
 										label="Environnements de test (dans l'audit)"
 										defaultExpanded
@@ -219,7 +263,6 @@ export const DeclarationAuditForm = withForm({
 																label={`Environnement ${index + 1} - Type`}
 																options={envKindOptions}
 																className={fr.cx("fr-mb-0")}
-																readOnly={readOnly}
 															/>
 														)}
 													</form.AppField>
@@ -248,92 +291,140 @@ export const DeclarationAuditForm = withForm({
 														)}
 													</form.Subscribe>
 												</div>
-												{!readOnly && (
+												<Button
+													type="button"
+													priority="secondary"
+													iconId="fr-icon-delete-bin-line"
+													onClick={() => field.removeValue(index)}
+													title="Supprimer l'environnement de test"
+												/>
+											</div>
+										))}
+										<Button
+											type="button"
+											onClick={() => field.pushValue({ kind: "", os: "" })}
+										>
+											Ajouter un environnement de test
+										</Button>
+									</Accordion>
+								) : (
+									<p>
+										<strong>Environnement de test :</strong>{" "}
+										{field.state.value.length > 0 ? (
+											<ul>
+												{field.state.value.map((value) => (
+													<li key={value.kind}>
+														{value.kind} - {value.os}
+													</li>
+												))}
+											</ul>
+										) : (
+											"None"
+										)}
+									</p>
+								)
+							}
+						</form.AppField>
+						<span
+							style={{
+								backgroundColor:
+									fr.colors.decisions.background.default.grey.hover,
+								height: "10px",
+								width: "100%",
+								marginBottom: fr.spacing("4w"),
+								display: "block",
+							}}
+						/>
+						<div className={fr.cx("fr-accordions-group")}>
+							<form.AppField name="audit.pages" mode="array">
+								{(field) =>
+									!readOnly ? (
+										<Accordion
+											label="Pages auditées"
+											defaultExpanded
+											className={classes.pagesAccordion}
+										>
+											{field.state.value.map((_, index) => (
+												<div
+													key={index}
+													className={classes.pagesAccordionContent}
+												>
+													<div className={classes.pagesWrapper}>
+														<form.AppField name={`audit.pages[${index}].label`}>
+															{(subField) => (
+																<subField.TextField
+																	label={`Page ${index + 1} - Label`}
+																	className={fr.cx("fr-mb-0")}
+																/>
+															)}
+														</form.AppField>
+														<form.AppField name={`audit.pages[${index}].url`}>
+															{(subField) => (
+																<subField.TextField
+																	label={`Page ${index + 1} - URL`}
+																	readOnly={readOnly}
+																/>
+															)}
+														</form.AppField>
+													</div>
 													<Button
 														type="button"
 														priority="secondary"
 														iconId="fr-icon-delete-bin-line"
 														onClick={() => field.removeValue(index)}
-														title="Supprimer l'environnement de test"
+														title="Supprimer la page"
 													/>
-												)}
-											</div>
-										))}
-										{!readOnly && (
+												</div>
+											))}
 											<Button
 												type="button"
-												onClick={() => field.pushValue({ kind: "", os: "" })}
+												onClick={() => field.pushValue({ url: "", label: "" })}
 											>
-												Ajouter un environnement de test
+												Ajouter une page
 											</Button>
-										)}
-									</Accordion>
-								)}
+										</Accordion>
+									) : (
+										<p>
+											<strong>Environnement de test :</strong>{" "}
+											{field.state.value.length > 0 ? (
+												<ul>
+													{field.state.value.map((value) => (
+														<li key={value.label}>
+															{value.label} - {value.url}
+														</li>
+													))}
+												</ul>
+											) : (
+												"None"
+											)}
+										</p>
+									)
+								}
 							</form.AppField>
-							<form.AppField name="audit.technologies" mode="array">
-								{(field) => (
-									<Accordion
-										label="Technologies utilisées (dans l'audit)"
-										defaultExpanded
-										className={classes.pagesAccordion}
-									>
-										{field.state.value.map((_, index) => (
-											<div
-												key={index}
-												className={classes.pagesAccordionContent}
-											>
-												<div className={classes.pagesWrapper}>
-													<form.AppField name={`audit.technologies[${index}]`}>
-														{(subField) => (
-															<subField.TextField
-																label={`Technologie ${index + 1}`}
-																readOnly={readOnly}
-															/>
-														)}
-													</form.AppField>
-												</div>
-												{!readOnly && (
-													<Button
-														type="button"
-														priority="secondary"
-														iconId="fr-icon-delete-bin-line"
-														onClick={() => field.removeValue(index)}
-														title="Supprimer la technologie"
-													/>
-												)}
-											</div>
-										))}
-										{!readOnly && (
-											<Button type="button" onClick={() => field.pushValue("")}>
-												Ajouter une technologie
-											</Button>
-										)}
-									</Accordion>
-								)}
-							</form.AppField>
+
 							<form.AppField name="audit.tools" mode="array">
-								{(field) => (
-									<Accordion
-										label="Outils utilisés (dans l'audit)"
-										defaultExpanded
-										className={classes.pagesAccordion}
-									>
-										{field.state.value.map((_, index) => (
-											<div
-												key={index}
-												className={classes.pagesAccordionContent}
-											>
-												<div className={classes.pagesWrapper}>
-													<form.AppField name={`audit.tools[${index}]`}>
-														{(subField) => (
-															<subField.TextField
-																label={`Outil ${index + 1}`}
-																readOnly={readOnly}
-															/>
-														)}
-													</form.AppField>
-												</div>
-												{!readOnly && (
+								{(field) =>
+									!readOnly ? (
+										<Accordion
+											label="Outils utilisés (dans l'audit)"
+											defaultExpanded
+											className={classes.pagesAccordion}
+										>
+											{field.state.value.map((_, index) => (
+												<div
+													key={index}
+													className={classes.pagesAccordionContent}
+												>
+													<div className={classes.pagesWrapper}>
+														<form.AppField name={`audit.tools[${index}]`}>
+															{(subField) => (
+																<subField.TextField
+																	label={`Outil ${index + 1}`}
+																	readOnly={readOnly}
+																/>
+															)}
+														</form.AppField>
+													</div>
 													<Button
 														type="button"
 														priority="secondary"
@@ -341,16 +432,27 @@ export const DeclarationAuditForm = withForm({
 														onClick={() => field.removeValue(index)}
 														title="Supprimer l'outil"
 													/>
-												)}
-											</div>
-										))}
-										{!readOnly && (
+												</div>
+											))}
 											<Button type="button" onClick={() => field.pushValue("")}>
 												Ajouter un outil
 											</Button>
-										)}
-									</Accordion>
-								)}
+										</Accordion>
+									) : (
+										<p>
+											<strong>Environnement de test :</strong>{" "}
+											{field.state.value.length > 0 ? (
+												<ul>
+													{field.state.value.map((value) => (
+														<li key={value}>{value}</li>
+													))}
+												</ul>
+											) : (
+												"None"
+											)}
+										</p>
+									)
+								}
 							</form.AppField>
 						</div>
 					</>
