c64c2a67e97da2058ba3ce4dae2dda0636fe1654
linnaa <linna.azaiez@gmail.com>
2026-02-02 16:28:43 +0100

    feat: add db migration

--- src/payload/migrations/index.ts
+++ src/payload/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20260129_145330 from './20260129_145330';
+import * as migration_20260202_152711_create_declaration_from_url from './20260202_152711_create_declaration_from_url';
 
 export const migrations = [
   {
     up: migration_20260129_145330.up,
     down: migration_20260129_145330.down,
-    name: '20260129_145330'
+    name: '20260129_145330',
+  },
+  {
+    up: migration_20260202_152711_create_declaration_from_url.up,
+    down: migration_20260202_152711_create_declaration_from_url.down,
+    name: '20260202_152711_create_declaration_from_url'
   },
 ];
