1673efd349cb1cac75eca73afa662718bac3c310
linnaa <linna.azaiez@gmail.com>
2026-02-02 18:20:18 +0100

    feat: add migration to change test environments field

--- src/payload/migrations/index.ts
+++ src/payload/migrations/index.ts
@@ -1,5 +1,6 @@
 import * as migration_20260129_145330 from './20260129_145330';
 import * as migration_20260202_152711_create_declaration_from_url from './20260202_152711_create_declaration_from_url';
+import * as migration_20260202_171943_change_test_environments_field from './20260202_171943_change_test_environments_field';
 
 export const migrations = [
   {
@@ -10,6 +11,11 @@ export const migrations = [
   {
     up: migration_20260202_152711_create_declaration_from_url.up,
     down: migration_20260202_152711_create_declaration_from_url.down,
-    name: '20260202_152711_create_declaration_from_url'
+    name: '20260202_152711_create_declaration_from_url',
+  },
+  {
+    up: migration_20260202_171943_change_test_environments_field.up,
+    down: migration_20260202_171943_change_test_environments_field.down,
+    name: '20260202_171943_change_test_environments_field'
   },
 ];
