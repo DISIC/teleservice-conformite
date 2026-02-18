91a47a21fb29ca081b95883f209cc76f66487dd4
Clément <clement@numericite.eu>
2026-02-18 12:21:49 +0100

    fix: responsive of readonly form

--- src/components/form/fields/ReadOnlyField.tsx
+++ src/components/form/fields/ReadOnlyField.tsx
@@ -4,88 +4,93 @@ import { tss } from "tss-react";
 import type { DefaultFieldProps } from "~/utils/form/context";
 
 interface ReadOnlyFieldProps extends DefaultFieldProps {
-	value: string | string[];
-	textArea?: boolean;
-	addSectionBorder?: boolean;
-	link?: boolean;
+  value: string | string[];
+  textArea?: boolean;
+  addSectionBorder?: boolean;
+  link?: boolean;
 }
 
 export function ReadOnlyField(props: ReadOnlyFieldProps) {
-	const {
-		label,
-		placeholder = "Non",
-		className,
-		value,
-		textArea = false,
-		addSectionBorder = false,
-		link = false,
-	} = props;
-	const valueIsArray = Array.isArray(value);
+  const {
+    label,
+    placeholder = "Non",
+    className,
+    value,
+    textArea = false,
+    addSectionBorder = false,
+    link = false,
+  } = props;
+  const valueIsArray = Array.isArray(value);
 
-	const { classes } = useStyles({
-		valueIsArray,
-		textArea,
-		addSectionBorder,
-	});
+  const { classes } = useStyles({
+    valueIsArray,
+    textArea,
+    addSectionBorder,
+  });
 
-	return (
-		<div className={classes.fieldContainer}>
-			<p className={classes.label}>{label} :</p>
-			{valueIsArray ? (
-				<ul className={classes.list}>
-					{value.map((item, index) => (
-						<li key={index}>
-							<p className={classes.value}>{item}</p>
-						</li>
-					))}
-				</ul>
-			) : (
-				<p className={classes.value}>
-					{link && value ? <a href={value}>{value}</a> : value || placeholder}
-				</p>
-			)}
-		</div>
-	);
+  return (
+    <div className={classes.fieldContainer}>
+      <p className={classes.label}>{label} :</p>
+      {valueIsArray ? (
+        <ul className={classes.list}>
+          {value.map((item, index) => (
+            <li key={index}>
+              <p className={classes.value}>{item}</p>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <p className={classes.value}>
+          {link && value ? <a href={value}>{value}</a> : value || placeholder}
+        </p>
+      )}
+    </div>
+  );
 }
 
 const useStyles = tss
-	.withName(ReadOnlyField.name)
-	.withParams<{
-		valueIsArray: boolean;
-		textArea: boolean;
-		addSectionBorder: boolean;
-	}>()
-	.create(({ valueIsArray, textArea, addSectionBorder }) => ({
-		fieldContainer: {
-			gap: fr.spacing("1v"),
-			paddingBlock: fr.spacing("3w"),
-			borderBottom: `1px solid ${fr.colors.decisions.border.default.grey.default}`,
-			"& p": {
-				margin: 0,
-			},
-			...(addSectionBorder && {
-				borderTop: `10px solid ${fr.colors.decisions.border.default.grey.default}`,
-			}),
-			...(valueIsArray || textArea
-				? {
-						display: "flex",
-						flexDirection: "column",
-					}
-				: {
-						display: "inline-flex",
-					}),
-		},
-		label: {
-			fontWeight: 700,
-			color: fr.colors.decisions.text.label.grey.default,
-		},
-		list: {},
-		value: {
-			color: fr.colors.decisions.text.label.grey.default,
-			whiteSpace: "pre-wrap",
+  .withName(ReadOnlyField.name)
+  .withParams<{
+    valueIsArray: boolean;
+    textArea: boolean;
+    addSectionBorder: boolean;
+  }>()
+  .create(({ valueIsArray, textArea, addSectionBorder }) => ({
+    fieldContainer: {
+      gap: fr.spacing("1v"),
+      paddingBlock: fr.spacing("3w"),
+      borderBottom: `1px solid ${fr.colors.decisions.border.default.grey.default}`,
+      "& p": {
+        margin: 0,
+      },
+      ...(addSectionBorder && {
+        borderTop: `10px solid ${fr.colors.decisions.border.default.grey.default}`,
+      }),
+      ...(valueIsArray || textArea
+        ? {
+            display: "flex",
+            flexDirection: "column",
+          }
+        : {
+            display: "inline-flex",
+          }),
 
-			"& a": {
-				color: fr.colors.decisions.text.actionHigh.blueFrance.default,
-			},
-		},
-	}));
+      "@media (max-width: 1024px)": {
+        flexDirection: "column",
+        gap: fr.spacing("2v"),
+      },
+    },
+    label: {
+      fontWeight: 700,
+      color: fr.colors.decisions.text.label.grey.default,
+    },
+    list: {},
+    value: {
+      color: fr.colors.decisions.text.label.grey.default,
+      whiteSpace: "pre-wrap",
+
+      "& a": {
+        color: fr.colors.decisions.text.actionHigh.blueFrance.default,
+      },
+    },
+  }));
