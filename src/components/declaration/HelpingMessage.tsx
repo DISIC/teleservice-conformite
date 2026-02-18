38d0d34fcae39da7cbb73266abb6790285ff140d
Clément <clement@numericite.eu>
2026-02-18 15:36:32 +0100

    fix: some a11y and first step form

--- src/components/declaration/HelpingMessage.tsx
+++ src/components/declaration/HelpingMessage.tsx
@@ -2,72 +2,74 @@ import { fr } from "@codegouvfr/react-dsfr";
 import { tss } from "tss-react";
 import Button from "@codegouvfr/react-dsfr/Button";
 import type {
-	FrIconClassName,
-	RiIconClassName,
+  FrIconClassName,
+  RiIconClassName,
 } from "@codegouvfr/react-dsfr/fr/generatedFromCss/classNames";
 
 interface HelpingMessageProps {
-	image: React.ReactNode;
-	message: string | React.ReactNode;
-	actionButtons?: {
-		label: string;
-		priority?: "primary" | "secondary";
-		iconId?: FrIconClassName | RiIconClassName;
-		onClick?: () => void;
-	}[];
+  image: React.ReactNode;
+  message: string | React.ReactNode;
+  actionButtons?: {
+    label: string;
+    priority?: "primary" | "secondary";
+    iconId?: FrIconClassName | RiIconClassName;
+    onClick?: () => void;
+  }[];
 }
 
 export default function HelpingMessage({
-	image,
-	message,
-	actionButtons,
+  image,
+  message,
+  actionButtons,
 }: HelpingMessageProps) {
-	const { classes } = useStyles();
+  const { classes } = useStyles();
 
-	return (
-		<div className={classes.helpingMessageContainer}>
-			{image}
-			<p className={classes.messageWrapper}>{message}</p>
-			<div className={classes.buttonsContainer}>
-				{actionButtons?.map((button) => (
-					<Button
-						key={button.label}
-						priority={button.priority || "primary"}
-						iconId={button.iconId as any}
-						onClick={button.onClick}
-					>
-						{button.label}
-					</Button>
-				))}
-			</div>
-		</div>
-	);
+  return (
+    <div className={classes.helpingMessageContainer}>
+      {image}
+      <p className={classes.messageWrapper}>{message}</p>
+      {!!actionButtons?.length && (
+        <div className={classes.buttonsContainer}>
+          {actionButtons?.map((button) => (
+            <Button
+              key={button.label}
+              priority={button.priority || "primary"}
+              iconId={button.iconId as any}
+              onClick={button.onClick}
+            >
+              {button.label}
+            </Button>
+          ))}
+        </div>
+      )}
+    </div>
+  );
 }
 
 const useStyles = tss.withName(HelpingMessage.name).create({
-	helpingMessageContainer: {
-		display: "grid",
+  helpingMessageContainer: {
+    display: "grid",
 
-		padding: fr.spacing("6v"),
-		marginBlock: fr.spacing("2w"),
-		gap: fr.spacing("6v"),
-		backgroundColor: fr.colors.decisions.background.contrast.blueFrance.default,
+    padding: fr.spacing("6v"),
+    marginBlock: fr.spacing("2w"),
+    gap: fr.spacing("6v"),
+    backgroundColor: fr.colors.decisions.background.contrast.blueFrance.default,
 
-		"@media (min-width: 830px)": {
-			gridTemplateColumns: "auto 2fr 1fr",
-		},
-	},
-	messageWrapper: {
-		display: "flex",
-		alignItems: "flex-start",
-		flexDirection: "column",
-		justifyContent: "center",
-		margin: 0,
-	},
-	buttonsContainer: {
-		display: "flex",
-		gap: fr.spacing("4w"),
-		alignItems: "center",
-		justifyContent: "flex-end",
-	},
+    "@media (min-width: 830px)": {
+      gridTemplateColumns: "auto auto 1fr",
+    },
+  },
+  messageWrapper: {
+    display: "flex",
+    alignItems: "flex-start",
+    flexDirection: "column",
+    justifyContent: "center",
+    margin: 0,
+  },
+  buttonsContainer: {
+    display: "flex",
+    gap: fr.spacing("4w"),
+    alignItems: "center",
+    justifyContent: "flex-end",
+  },
 });
--- src/utils/form/declaration/form.tsx
+++ src/utils/form/declaration/form.tsx
@@ -8,200 +8,208 @@ import { useRef, useEffect, useState } from "react";
 
 import { appKindOptions, kindOptions } from "~/payload/selectOptions";
 import { withForm } from "../context";
-import { declarationMultiStepFormOptions } from "./schema";
+import {
+  declarationMultiStepFormOptions,
+  type ZInitialDeclaration,
+} from "./schema";
 import HelpingMessage from "~/components/declaration/HelpingMessage";
 
+type DeclarationKind =
+  ZInitialDeclaration["initialDeclaration"]["newDeclarationKind"];
+
 export const DeclarationGeneralForm = withForm({
-	...declarationMultiStepFormOptions,
-	props: { readOnly: false },
-	render: function Render({ form, readOnly }) {
-		return (
-			<div>
-				<form.AppField name="general.organisation">
-					{(field) => (
-						<field.TextField
-							label="Organisation"
-							readOnly={readOnly}
-							inputReadOnly
-						/>
-					)}
-				</form.AppField>
-				<form.AppField name="general.kind">
-					{(field) => (
-						<field.RadioField
-							label="Type de service"
-							options={[...appKindOptions]}
-							readOnly={readOnly}
-							onChange={() => {
-								form.setFieldValue("general.url", "");
-							}}
-						/>
-					)}
-				</form.AppField>
-				<form.AppField name="general.name">
-					{(field) => (
-						<field.TextField
-							label="Nom de la déclaration"
-							readOnly={readOnly}
-							description={
-								<>
-									Nous vous conseillons d’utiliser le nom du service numérique.
-									<br />
-									Exemples : Demande de logement social, Service public.fr,
-									Outil de gestion des congés
-								</>
-							}
-						/>
-					)}
-				</form.AppField>
-				<form.Subscribe selector={(store) => store.values.general?.kind}>
-					{(kind) =>
-						kind === "website" ? (
-							<form.AppField name="general.url">
-								{(field) => (
-									<field.TextField
-										label="URL du service (facultatif)"
-										kind="url"
-										description={
-											<>
-												Pour un site web public, renseignez l’adresse accessible
-												par les usagers.
-												<br />
-												Format attendu : https://www.monservice.gouv.fr
-											</>
-										}
-										readOnly={readOnly}
-									/>
-								)}
-							</form.AppField>
-						) : null
-					}
-				</form.Subscribe>
-				<form.AppField name="general.domain">
-					{(field) => (
-						<field.SelectField
-							label="Secteur d’activité de l’organisation"
-							placeholder="Sélectionnez un secteur"
-							infoStateMessage="Si vous représentez une agglomération, choisissez “Aucun de ces domaines”"
-							readOnly={readOnly}
-							options={[...kindOptions]}
-						/>
-					)}
-				</form.AppField>
-			</div>
-		);
-	},
+  ...declarationMultiStepFormOptions,
+  props: { readOnly: false },
+  render: function Render({ form, readOnly }) {
+    return (
+      <div>
+        <form.AppField name="general.organisation">
+          {(field) => (
+            <field.TextField
+              label="Organisation"
+              readOnly={readOnly}
+              inputReadOnly
+            />
+          )}
+        </form.AppField>
+        <form.AppField name="general.kind">
+          {(field) => (
+            <field.RadioField
+              label="Type de service"
+              options={[...appKindOptions]}
+              readOnly={readOnly}
+              onChange={() => {
+                form.setFieldValue("general.url", "");
+              }}
+            />
+          )}
+        </form.AppField>
+        <form.AppField name="general.name">
+          {(field) => (
+            <field.TextField
+              label="Nom de la déclaration"
+              readOnly={readOnly}
+              description={
+                <>
+                  Nous vous conseillons d’utiliser le nom du service numérique.
+                  <br />
+                  Exemples : Demande de logement social, Service public.fr,
+                  Outil de gestion des congés
+                </>
+              }
+            />
+          )}
+        </form.AppField>
+        <form.Subscribe selector={(store) => store.values.general?.kind}>
+          {(kind) =>
+            kind === "website" ? (
+              <form.AppField name="general.url">
+                {(field) => (
+                  <field.TextField
+                    label="URL du service (facultatif)"
+                    kind="url"
+                    description={
+                      <>
+                        Pour un site web public, renseignez l’adresse accessible
+                        par les usagers.
+                        <br />
+                        Format attendu : https://www.monservice.gouv.fr
+                      </>
+                    }
+                    readOnly={readOnly}
+                  />
+                )}
+              </form.AppField>
+            ) : null
+          }
+        </form.Subscribe>
+        <form.AppField name="general.domain">
+          {(field) => (
+            <field.SelectField
+              label="Secteur d’activité de l’organisation"
+              placeholder="Sélectionnez un secteur"
+              infoStateMessage="Si vous représentez une agglomération, choisissez “Aucun de ces domaines”"
+              readOnly={readOnly}
+              options={[...kindOptions]}
+            />
+          )}
+        </form.AppField>
+      </div>
+    );
+  },
 });
 
 export const ContextForm = withForm({
-	...declarationMultiStepFormOptions,
-	render: function Render({ form }) {
-		const { classes } = useStyles();
-		const [newDeclaration, setNewDeclaration] = useState("");
+  ...declarationMultiStepFormOptions,
+  render: function Render({ form }) {
+    const { classes } = useStyles();
+    const [newDeclarationKind, setNewDeclarationKind] =
+      useState<DeclarationKind>(undefined);
 
-		const textFieldContainerRef = useRef<HTMLDivElement | null>(null);
-		useEffect(() => {
-			if (
-				textFieldContainerRef.current &&
-				["viaUrl", "viaAra"].includes(newDeclaration)
-			) {
-				textFieldContainerRef.current.scrollIntoView({ behavior: "smooth" });
+    return (
+      <div className={classes.contextFormContainer}>
+        <form.AppField name="initialDeclaration.newDeclarationKind">
+          {(field) => (
+            <field.SelectCardField
+              name="initialDeclaration.newDeclarationKind"
+              label="Quelle est votre situation pour ce service ?"
+              options={[
+                {
+                  id: "fromUrl",
+                  label:
+                    "J'ai une déclaration en ligne, sans avoir utilisé Ara",
+                  description:
+                    "La nouvelle déclaration pourra être pré-remplie grâce à une IA souveraine; des erreurs peuvent survenir",
+                  image: <Internet fontSize="3rem" />,
+                },
+                {
+                  id: "fromAra",
+                  label:
+                    "J'ai une déclaration en ligne réalisée avec l’outil Ara",
+                  description:
+                    "La nouvelle déclaration pourra être pré-remplie automatiquement",
+                  image: <System fontSize="3rem" />,
+                },
+                {
+                  id: "fromScratch",
+                  label: "Je n’ai pas de déclaration d’accessibilité",
+                  description:
+                    "La nouvelle déclaration sera à créer manuellement",
+                  image: <DocumentSearch fontSize="3rem" />,
+                },
+              ]}
+              onChange={(value) => {
+                form.resetField("initialDeclaration.declarationUrl");
+                form.resetField("initialDeclaration.araUrl");
 
-				setTimeout(() => {
-					const el = textFieldContainerRef.current?.querySelector(
-						"input, textarea",
-					) as HTMLInputElement | HTMLTextAreaElement | null;
-					el?.focus();
-				}, 200);
-			}
-		}, [newDeclaration]);
-
-		return (
-			<div className={classes.contextFormContainer}>
-				<form.AppField name="initialDeclaration.newDeclaration">
-					{(field) => (
-						<field.SelectCardField
-							name="initialDeclaration.newDeclaration"
-							label="Quelle est votre situation pour ce service ?"
-							options={[
-								{
-									id: "viaUrl",
-									label:
-										"J'ai une déclaration en ligne, sans avoir utilisé Ara",
-									description:
-										"La nouvelle déclaration pourra être pré-remplie grâce à une IA souveraine; des erreurs peuvent survenir",
-									image: <Internet fontSize="3rem" />,
-								},
-								{
-									id: "viaAra",
-									label:
-										"J'ai une déclaration en ligne réalisée avec l’outil Ara",
-									description:
-										"La nouvelle déclaration pourra être pré-remplie automatiquement",
-									image: <System fontSize="3rem" />,
-								},
-								{
-									id: "new",
-									label: "Je n’ai pas de déclaration d’accessibilité",
-									description:
-										"La nouvelle déclaration sera à créer manuellement",
-									image: <DocumentSearch fontSize="3rem" />,
-								},
-							]}
-							onChange={(value) => {
-								form.resetField("initialDeclaration.declarationUrl");
-								form.resetField("initialDeclaration.araUrl");
-
-								setNewDeclaration(value);
-							}}
-						/>
-					)}
-				</form.AppField>
-				<form.Subscribe
-					selector={(store) => store.values.initialDeclaration?.newDeclaration}
-				>
-					{(newDeclaration) => (
-						<div ref={textFieldContainerRef}>
-							{newDeclaration === "viaUrl" && (
-								<form.AppField name="initialDeclaration.declarationUrl">
-									{(field) => (
-										<field.TextField
-											label="Lien URL de la déclaration en ligne "
-											kind="url"
-											description="Format attendu : https://www.example.fr"
-										/>
-									)}
-								</form.AppField>
-							)}
-							{newDeclaration === "viaAra" && (
-								<form.AppField name="initialDeclaration.araUrl">
-									{(field) => (
-										<field.TextField
-											label="URL de l’audit Ara"
-											kind="url"
-											description="Format attendu : https://ara.numerique.gouv.fr/declaration/xxxxxxx"
-										/>
-									)}
-								</form.AppField>
-							)}
-							{newDeclaration === "" && (
-								<HelpingMessage
-									image={<Accessibility fontSize="6rem" />}
-									message="Ara est un outil destiné aux auditeurs qui permet de réaliser un rapport d’audit complet et de générer automatiquement une déclaration d’accessibilité. "
-								/>
-							)}
-						</div>
-					)}
-				</form.Subscribe>
-			</div>
-		);
-	},
+                setNewDeclarationKind(value as DeclarationKind);
+              }}
+            />
+          )}
+        </form.AppField>
+        {newDeclarationKind !== "fromScratch" && (
+          <form.Subscribe
+            selector={(store) =>
+              store.values.initialDeclaration?.newDeclarationKind
+            }
+          >
+            {(newDeclarationKind) => (
+              <div>
+                {newDeclarationKind === "fromUrl" && (
+                  <form.AppField name="initialDeclaration.declarationUrl">
+                    {(field) => (
+                      <field.TextField
+                        label="Lien URL de la déclaration en ligne "
+                        kind="url"
+                        description="Format attendu : https://www.example.fr"
+                      />
+                    )}
+                  </form.AppField>
+                )}
+                {newDeclarationKind === "fromAra" && (
+                  <form.AppField name="initialDeclaration.araUrl">
+                    {(field) => (
+                      <field.TextField
+                        label="URL de l’audit Ara"
+                        kind="url"
+                        description="Format attendu : https://ara.numerique.gouv.fr/declaration/xxxxxxx"
+                      />
+                    )}
+                  </form.AppField>
+                )}
+                {!newDeclarationKind && (
+                  <HelpingMessage
+                    image={<Accessibility fontSize="6rem" />}
+                    message={
+                      <span>
+                        <a
+                          href="https://ara.numerique.gouv.fr/"
+                          className={fr.cx("fr-link")}
+                          target="_blank"
+                          rel="noreferrer"
+                        >
+                          Ara
+                        </a>{" "}
+                        est un outil destiné aux auditeurs qui permet de
+                        réaliser un rapport d’audit complet et de générer
+                        automatiquement une déclaration d’accessibilité.
+                      </span>
+                    }
+                  />
+                )}
+              </div>
+            )}
+          </form.Subscribe>
+        )}
+      </div>
+    );
+  },
 });
 
 const useStyles = tss.withName(ContextForm.name).create({
-	contextFormContainer: {
-		display: "flex",
-		flexDirection: "column",
-		gap: "2rem",
-	},
+  contextFormContainer: {
+    display: "flex",
+    flexDirection: "column",
+    gap: "2rem",
+  },
 });
