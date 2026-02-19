831588e31666bf1ace6916299bb9a278c9134f1e
Antoine Lelong <azenoxe@gmail.com>
2026-02-19 14:24:19 +0100

    fix: add correct payloadcms/email-nodemailer version to match current payload version and add migrations

--- src/payload/migrations/index.ts
+++ src/payload/migrations/index.ts
@@ -3,6 +3,7 @@ import * as migration_20260202_152711_create_declaration_from_url from './202602
 import * as migration_20260202_171943_change_test_environments_field from './20260202_171943_change_test_environments_field';
 import * as migration_20260204_093440_add_published_content from './20260204_093440_add_published_content';
 import * as migration_20260212_132442_update_audit_fields from './20260212_132442_update_audit_fields';
+import * as migration_20260219_132340 from './20260219_132340';
 
 export const migrations = [
   {
@@ -28,6 +29,11 @@ export const migrations = [
   {
     up: migration_20260212_132442_update_audit_fields.up,
     down: migration_20260212_132442_update_audit_fields.down,
-    name: '20260212_132442_update_audit_fields'
+    name: '20260212_132442_update_audit_fields',
+  },
+  {
+    up: migration_20260219_132340.up,
+    down: migration_20260219_132340.down,
+    name: '20260219_132340'
   },
 ];
