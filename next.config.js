6860ae3bdd08e8886fb1b9199b4d29b6f9f2d050
Antoine <azenoxe@gmail.com>
2026-05-20 14:30:58 +0200

    fix: remove custom webpack in nextjs.config file

--- next.config.js
+++ next.config.js
@@ -3,13 +3,7 @@ import { withPayload } from "@payloadcms/next/withPayload";
 /** @type {import("next").NextConfig} */
 const config = {
 	reactStrictMode: true,
-	webpack: (config) => {
-		config.module.rules.push({
-			test: /\.woff2$/,
-			type: "asset/resource",
-		});
-		return config;
-	},
+	turbopack: {},
 	transpilePackages: [
 		"@codegouvfr/react-dsfr",
 		"tss-react", // This is for MUI or if you use htts://tss-react.dev
